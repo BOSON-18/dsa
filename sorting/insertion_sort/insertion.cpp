5211923d49af1e6ad7466fe353118a09fb0fbc04
Boson-18 <deeshankbatra663@gmail.com>
2026-03-01 16:09:50 +0530

    Selection and Insertion Sort

---
+++ sorting/insertion_sort/insertion.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(vector<int>& arr){
+  int n=arr.size();
+	for(int i=1;i<n;i++){
+		int key = arr[i];
+		int j=i-1;
+		
+		while( j>=0 && arr[j] > key){
+			arr[j+1] = arr[j];
+			j=j-1;
+		}
+		arr[j+1] = key;
+	}
+}
+
+void printArray(vector<int>& arr){
+	for(int it:arr){
+		cout<<" "<<it;
+	}
+	cout<<endl;
+}
+
+int main(){
+	vector<int> arr = {12,11,13,5,6};
+	
+	insertionSort(arr);
+	printArray(arr);
+	
+	return 0;
+}
